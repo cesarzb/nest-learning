9e6488bfbe7c60ab32769fe6f66d677ea0e61180
Bernard Cesarz <cesarzb@protonmail.com>
2025-12-29 13:32:16 +0100

    Products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ProductsController } from './products/products.controller';
-import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [ProductsController],
-  providers: [ProductsService],
+  imports: [ProductsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
